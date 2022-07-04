1a6541dcd59d035f8856115fe4a8ccf4df252d32
Varia S <especificita@gmail.com>
2022-07-04 17:12:38 +0300

    Added Task 21

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-Console.WriteLine ("Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+/*Console.WriteLine ("Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
 Console.WriteLine("Введите пятизначное число: ");
 string n = Console.ReadLine();
 void Task19 (string arg)
@@ -21,4 +21,25 @@ void Task19 (string arg)
         Console.WriteLine("Введите все-таки пятизначное число");
     }
 }
-Task19(n);
+Task19(n);*/
+
+void Task21()
+{
+    Console.WriteLine("Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
+    Console.WriteLine("Введите первую координату по оси Х: ");
+    int x1 = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine("Введите первую координату по оси Y: ");
+    int y1 = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine("Введите первую координату по оси Z: ");
+    int z1 = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine("Введите вторую координату по оси Х: ");
+    int x2 = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine("Введите вторую координату по оси Y: ");
+    int y2 = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine("Введите вторую координату по оси Z: ");
+    int z2 = Convert.ToInt32 (Console.ReadLine ());
+    double result = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2) + Math.Pow(z2-z1,2));
+    Console.WriteLine(result);
+    
+}
+Task21 ();
